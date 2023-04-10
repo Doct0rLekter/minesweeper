fcb4a16462c685e15f270a9191b3bebd54ab40e0
Doct0rLekter <marikwinters@gmail.com>
2023-04-10 15:05:04 -0400

    Fix various mutability and ownership errors surrounding 'GameState' struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-// Start blocking out a framework
+// Set clippy to pedantic
+#![warn(clippy::pedantic)]
 
+// Start blocking out a framework
 // The below framework is loosely based on learnings from many sources on the 'game loop' design pattern
 // The primary source referenced during the building of this framework is: https://www.gameprogrammingpatterns.com/game-loop.html
 // This, of course, has to be translated into the world of rust
 mod game_loop {
-    
+
     // Provide structure to game data
     pub struct GameState {
         pub input: String,
@@ -13,7 +15,6 @@ mod game_loop {
 
     impl GameState {
         pub fn new() -> GameState {
-            
             let state = GameState {
                 input: "".to_string(),
                 game_over: false,
@@ -22,69 +23,66 @@ mod game_loop {
         }
 
         pub fn get_input(&self) -> String {
-            self.input
+            let input = self.input.clone();
+            input
         }
 
         pub fn get_game_over(&self) -> bool {
-            self.game_over            
+            self.game_over
         }
 
-        pub fn set_input(&self, input: String) {
+        pub fn set_input(&mut self, input: String) {
             self.input = input;
         }
 
-        pub fn set_game_over(&self, game_over: bool) {
+        pub fn set_game_over(&mut self, game_over: bool) {
             self.game_over = game_over;
         }
     }
 
-
     pub fn play() {
-        let mut state = GameState::new();    
+        let mut state = GameState::new();
         // Reset the game state after a game over
-        reset(&state);
-        
+        reset(&mut state);
+
         // Draw the initial game state
         draw();
-        
+
         loop {
             let game_over = state.get_game_over();
 
             if game_over {
                 break;
             }
-            
+
             // Process console input
             process_input();
-            
+
             // Update the game state
-                update();
-                
-                // Redraw game state after each update
-                draw();
-            }
-        }
-        
-        fn reset(state: &GameState) {
-            state.set_input("".to_string());
-            state.set_game_over(false);
-        }
-        
-        fn process_input() {
-            TODO
-        }
-        
-        fn update() {
-            TODO
-        }
-        
-        fn draw() {
-            TODO
+            update();
+
+            // Redraw game state after each update
+            draw();
         }
     }
 
+    fn reset(state: &mut GameState) {
+        state.set_input("".to_string());
+        state.set_game_over(false);
+    }
 
+    fn process_input() {
+        // TODO
+    }
 
+    fn update() {
+        // TODO
+    }
+
+    fn draw() {
+        // TODO
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -93,14 +91,13 @@ mod test {
     #[test]
     fn two_plus_two() {
         assert_eq!(4, (2 + 2))
-    } 
+    }
 
-    #[test]   
+    #[test]
     fn gets_input() {
         let state = game_loop::GameState::new();
 
-        assert_eq!(state.input, state.get_input());  
-        
+        assert_eq!(state.input, state.get_input());
     }
 
     #[test]
@@ -112,7 +109,7 @@ mod test {
 
     #[test]
     fn sets_input() {
-        let state = game_loop::GameState::new();
+        let mut state = game_loop::GameState::new();
         state.set_input("See? I can set input".to_string());
 
         assert_eq!("See? I can set input".to_string(), state.input);
@@ -120,9 +117,9 @@ mod test {
 
     #[test]
     fn sets_game_over() {
-        let state = game_loop::GameState::new();
+        let mut state = game_loop::GameState::new();
         state.set_game_over(true);
 
-        assert_eq!(false, state.game_over)
+        assert_eq!(true, state.game_over)
     }
 }
