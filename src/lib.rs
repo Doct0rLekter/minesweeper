0dd58091bef775936f797a607ecdab0697668675
Doct0rLekter <marikwinters@gmail.com>
2023-04-10 10:55:08 -0400

    Block out basic game loop - Add 'play' function block out - Add 'reset', 'process_input', 'update', and 'draw' function block outs - Add GameState struct to store data pertaining to the game state - Group game loop elements under 'game_loop' module - Block out 'game_loop::play()' call in main function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,65 @@
+// Start blocking out a framework
 
+// The below framework is loosely based on learnings from many sources on the 'game loop' design pattern
+// The primary source referenced during the building of this framework is: https://www.gameprogrammingpatterns.com/game-loop.html
+// This, of course, has to be translated into the world of rust
+/*
+
+mod game_loop {
+    
+    // Provide structure to game data
+
+    struct GameState {
+        TODO
+    }
+
+    impl GameState {
+        TODO
+    }
+
+    play() {
+        TODO
+    
+        // Reset the game state after a game over
+        reset();
+    
+        // Draw the initial game state
+        draw();
+    
+        loop {
+            if game_over {
+                break;
+            }
+    
+            // Process console input
+            process_input();
+    
+            // Update the game state
+            update();
+    
+            // Redraw game state after each update
+            draw();
+        }
+    }
+    
+    Reset() {
+        TODO
+    }
+    
+    process_input() {
+        TODO
+    }
+    
+    update() {
+        TODO
+    }
+    
+    draw() {
+        TODO
+    }
+}
+
+*/
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// Set clippy to pedantic
+#![warn(clippy::pedantic)]
+
+// use minesweeper::{self, game_loop};
+
 fn main() {
+    // More block outs
+    /*
+    TODO
+    game_loop::play()
+    */
+
     println!("Hello, world!");
 }
