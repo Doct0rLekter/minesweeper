594c11432ec25308fcff9d1100426be40c3006f0
Doct0rLekter <marikwinters@gmail.com>
2023-04-27 13:40:36 -0400

    Minor formatting fixes

--- src/lib.rs
+++ src/lib.rs
@@ -235,7 +235,6 @@ pub mod game_loop {
         neighbors
     }
 
-    
     fn calculate_hint(state: &GameState, index: usize) -> u32 {
         let neighbors = find_neighbors(state, index);
 
@@ -459,7 +458,7 @@ pub mod game_loop {
             // Consider adding end of game stats
             println!("Game over!");
         }
-        
+
         if state.get_won() {
             println!("Congratulations, you found all of the mines!");
         }
